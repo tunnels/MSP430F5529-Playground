474321e3c8825a2cd4407403de6f61b212027c8b
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2017-12-10 18:42:58 -0500

    holding button P1.1 activates a red LED

---
+++ assembly/button_hold.asm
@@ -0,0 +1,55 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+ BIC #0xFF, &P1DIR ; assign all PA things as input
+ BIC #0xFF, &P2DIR
+
+ BIS.B #1, &P1DIR ; assign P1.0 as output/light
+
+ BIS.B #2, &P1REN
+ BIS.B #2, &P1OUT ; set P1.1 to be a pull-up resister
+
+BTNOFF:
+ BIS.B #1, &P1OUT
+LOOP:
+ BIT.B #2, &P1IN
+ JZ BTNOFF ; if false, go back to loop
+
+ BIC.B #1, &P1OUT
+ JMP LOOP
+ NOP
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
