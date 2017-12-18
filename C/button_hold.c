e8922116c415a2b43e5c23df9771e9b214d942b0
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2017-12-17 20:33:23 -0500

    added support for the other button

--- C/button_hold.c
+++ C/button_hold.c
@@ -5,18 +5,31 @@ void main(void)
 	// stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
 
-	// set P1.0 to be output and P1.1 to be input
+	// set P1.0 to be LED1 (red) and P1.1 to be button1
 	P1DIR = BIT0 & ~BIT1;
 
-	// set P1.1 as a pull-up resistor
-	P1REN = BIT1;
-	P1OUT = BIT1;
+	// set P2.1 as button2
+	P2DIR &= ~BIT1;
 
-	while (1 > 0) {
-		// if P1.1 is not pressed; if P1.1 is high voltage
+	// set P4.7 as LED2 (green)
+	P4DIR |= BIT7;
+
+	// set button1 and button2 as a pull-up resistors
+	P2REN = P1REN = BIT1;
+	P2OUT = P1OUT = BIT1;
+
+	while (1) {
+		// if button1 is not pressed; if P1.1 is high voltage
 		if ((P1IN & 0002) == BIT1)
 			P1OUT &= ~BIT0;
-		else 
+		else {
 			P1OUT |= BIT0;
+		}
+
+		// likewise for button2
+		if ((P2IN & 0002) == BIT1)
+			P4OUT &= ~BIT7;
+		else 
+			P4OUT |= BIT7;		
 	}
 }
