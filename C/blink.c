9328f15f0394c0c03f8603696108d017e7fd7fdb
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2017-12-19 19:10:59 -0500

    buttons do stuff now

--- C/blink.c
+++ C/blink.c
@@ -1,53 +1,51 @@
 #include <msp430.h>				
 
-void alternateLights(void);
+void turnOffBothLEDs();
 void main(void)
  {
 	// stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
-
-	// set P1.0 to LED_1 (red) and P1.1 to be button1
-	// set P2.1 as button2
-	// set P4.7 as LED_2 (green)
-	P1DIR = BIT0 & ~BIT1;
+	
+	// P1.1 and P2.1 are the buttons
+	P1DIR &= ~BIT1;
 	P2DIR &= ~BIT1;
+	
+	// P1.0 = red LED; P4.7 = green LED
+	P1DIR |= BIT0;
 	P4DIR |= BIT7;
 	
 	// set button1 and button2 as pull-up resistors
 	P2REN = P1REN = BIT1;
 	P2OUT = P1OUT = BIT1;
 
-	while (1) {
-		// turn off LEDs at each loop start
-		P1OUT &= ~BIT1;
-		P4OUT &= ~BIT7;
+	turnOffBothLEDs();
 
-		// P2.1 is low-voltage, which means button2 was pressed
-		if ((P2IN & 0x0002) != BIT1) {
+	while (1) {
+		// if button2 is pressed
+		if ((P2IN & BIT1) != BIT1) {
 			alternateLights();
 		}
 	}
 }
 
-// alternates between lights 5 times
-void alternateLights(void)
-{
-	volatile unsigned int blink_lights = 1;
-	int i = 0;
-
-	while (blink_lights) {
-		// alternate LED_1
+void alternateLights()
+{	
+	while (1) {
 		P1OUT ^= BIT0;
-
-		// delay
-		for(i = 30000; i > 0; i--);
-		
-		// alternate LED_2
+		__delay_cycles(100000);
 		P4OUT ^= BIT7;
+		__delay_cycles(100000);
 
-		// // if button1 is pressed
-		// if ((P1IN & 0x0002) == BIT1) {
-		// 	blink_lights = 0;
-		// }
+		// if button1 is pressed
+		if ((P1IN & BIT1) != BIT1) {
+			turnOffBothLEDs();
+			return;
+		}
 	}
+}
+
+void turnOffBothLEDs()
+{
+	P1OUT &= ~BIT0;
+	P4OUT &= ~BIT7;	
 }
